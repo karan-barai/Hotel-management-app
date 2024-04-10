1762e7b92c7b5dc8f37a0dcff54842b61521c7d5
Karan Barai <135259079+karan-barai@users.noreply.github.com>
2024-04-10 20:39:37 +0530

    email and password

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -6,8 +6,9 @@ export default function Hero() {
     <div className="hero-content text-center">
       <div className="max-w-md">
         <h1 className="text-3xl font-bold">Hotel Management APP</h1>
-        {/* <p className="py-6">Click on SignIn/SignUp to use the app.</p> */}
-    
+        <p className="py-6">Sign in details</p>
+        <p className="py-6">Email: manager@test.com</p>
+        <p className="py-6">Password: manager@test.com</p>
       </div>
     </div>
   </div>
